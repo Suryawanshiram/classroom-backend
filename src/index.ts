e6653ee7ebac44b477abe0535af8220c415abf69
Suryawanshiram <suryawanshiram2@gmail.com>
2026-01-16 18:09:23 +0530

    add: implemented db schemas

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import express from "express";
 const app = express();
 const PORT = 8000;
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello, welcome to the Classroom API!");
 });
